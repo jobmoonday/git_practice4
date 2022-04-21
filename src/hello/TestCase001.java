b22143b703fa847e738d941fbfc6498063a18ba6
eyup <jobmoonday2@gmail.com>
2022-04-21 05:05:56 +0100

    new step 2

--- src/hello/TestCase001.java
+++ src/hello/TestCase001.java
@@ -4,5 +4,8 @@ public class TestCase001 {
     public static void main(String[] args) {
         //main method added
         System.out.println("New function");
+        // step 2 added
+        int a=2;
+
     }
 }
